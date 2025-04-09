c2db002b8d043a178364b0c56d283dddb3f78cc3
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-04-08 21:56:59 -0300

    feat: create and impl Product.cs

---
+++ WebMvc_oracle/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebMvc_oracle.Models
+{
+    [Table("tblProduto")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "Code")]
+        private int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name = "Name")]
+        private string? Name { get; set; }
+    }
+}
